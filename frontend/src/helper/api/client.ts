a349e39e3ce6c9b318da86c473dcbe6b905c6aa3
grilhami <gilangrilhami@hotmail.co.uk>
2022-09-10 12:20:48 +0800

    Add api connection support

---
+++ frontend/src/helper/api/client.ts
@@ -0,0 +1,16 @@
+import { getAxiosInstance } from ".";
+import { IClientGet, IClientPost } from "./interfaces";
+
+export const getClients = async () => {
+  const api = getAxiosInstance()
+  const response = await api.get("/api/v1/clients");
+  const data: IClientGet[] = response.data;
+  return data;
+};
+
+export const createClient = async (payload: IClientPost) => {
+  const api = getAxiosInstance()
+  const response = await api.post("/api/v1/clients", payload);
+  const data: IClientGet = response.data;
+  return data;
+};
---
+++ frontend/src/helper/api/interfaces/client.ts
@@ -0,0 +1,15 @@
+export interface IClientGet {
+    id: string;
+    name: string;
+    size: string;
+    address: string;
+    phone: string;
+    _ts: string;
+}
+
+export interface IClientPost {
+    name: string;
+    size: string;
+    address: string;
+    phone: string;
+}
